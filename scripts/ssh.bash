86b2dd51abd674dec586ddfb4574c7c914cf7f86
Sean Fisk <sean@seanfisk.com>
2013-04-29 16:00:16 -0400

    Add SSH notes and comments.

--- scripts/ssh.bash
+++ scripts/ssh.bash
@@ -5,9 +5,9 @@ ssh-keygen
 # Add new SSH key to authorized_keys
 < ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 _trust_host() { ssh -o StrictHostKeyChecking=no -o ConnectTimeout=2 "$1" exit }
-# trust all EOS machines
+# Trust all EOS machines
 for i in {1..24}; do _trust_host "eos$(printf '%02d' $i)"; done
-# trust all Arch machines
+# Trust all Arch machines
 for i in {1..8}; do _trust_host "arch$(printf '%02d' $i)"; done
 
-ssh eos15 # automatic login
+ssh eos15 # Automatic login
