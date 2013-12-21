f766aa51290b694f49dd0887fb58b606a6269053
Sean Fisk <sean@seanfisk.com>
2013-12-21 01:09:23 -0500

    Align comments in scripts.

--- scripts/install-aria2.bash
+++ scripts/install-aria2.bash
@@ -1,11 +1,11 @@
-mkdir -p ~/.local/src # create directory for source code
+mkdir -p ~/.local/src                           # create directory for source code
 cd ~/.local/src
 wget 'http://downloads.sourceforge.net/project/'\
 'aria2/stable/aria2-1.17.0/aria2-1.17.0.tar.gz' # download aria2's source code archive
-tar -xf aria2-1.17.0.tar.gz # unarchive source code
+tar -xf aria2-1.17.0.tar.gz                     # unarchive source code
 cd aria2-1.17.0
-./configure --prefix ~/.local # set up build configuration
-make # build the program
-make install # install program to ~/.local/bin
-export PATH=~/.local/bin:$PATH # consider adding to ~/.bash_profile
+./configure --prefix ~/.local                   # set up build configuration
+make                                            # build the program
+make install                                    # install program to ~/.local/bin
+export PATH=~/.local/bin:$PATH                  # consider adding to ~/.bash_profile
 aria2c 'http://www.irs.gov/pub/irs-pdf/fw4.pdf' # download the W4
