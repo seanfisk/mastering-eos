92ee998fe31634f9d8340647320b242c5d31f72e
Sean Fisk <sean@seanfisk.com>
2014-02-13 14:59:42 -0500

    Switch to deploying with fabric.

--- manual/fabfile.py
+++ manual/fabfile.py
@@ -1,4 +1,6 @@
 from __future__ import print_function
+from glob import glob
+import subprocess
 
 from fabric.api import env, task, execute, run, runs_once
 from texttable import Texttable
@@ -12,14 +14,16 @@ if env.hosts == []:
 
 
 @task
-def per_machine():
+def fingerprint_for_machine():
+    """Find the SSH fingerprint for a specific machine."""
     return run('ssh-keygen -l -f /etc/ssh/ssh_host_rsa_key.pub', shell=False)
 
 
 @task
 @runs_once
-def fingerprints():
-    results_dict = execute(per_machine)
+def gen_fingerprints():
+    """Generate a fingerprints table for the documentation."""
+    results_dict = execute(fingerprint_for_machine)
     table = Texttable()
     # The default decoration produces the correct table.
     table.header(['Host', 'Fingerprint'])
@@ -31,3 +35,32 @@ def fingerprints():
 
     with open('ssh/common/fingerprints.rst', 'w') as fingerprint_file:
         print(table.draw(), file=fingerprint_file)
+
+
+@task
+@runs_once
+def build():
+    """Build the manual."""
+    # Must have epub first or we receive 'index can't be built errors'.
+
+    # We choose not to use fabric's local() because it uses the shell. It's
+    # better to avoid invoking the shell.
+    subprocess.check_call(['make', 'epub', 'html', 'latexpdf', 'man', 'info'])
+
+
+@task
+@runs_once
+def deploy():
+    """Deploy the manual to the user's personal EOS account."""
+    subprocess.check_call([
+        'rsync',
+        '--verbose',
+        '--archive',
+        '--chmod=go=rX',
+        '--compress'
+    ] + glob('_build/html/*') + [
+        '_build/latex/MasteringEOS.pdf',
+        '_build/epub/MasteringEOS.epub',
+        # Allow the user to override the username using fabric.
+        env.user + '@eos10.cis.gvsu.edu:public_html/mastering-eos'
+    ])
