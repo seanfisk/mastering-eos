145a97c085fcf4637065de1ce3023cbd8ce12fbc
Sean Fisk <sean@seanfisk.com>
2014-02-13 15:14:34 -0500

    Fix man page section and man and info file names.

--- manual/conf.py
+++ manual/conf.py
@@ -229,8 +229,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'masteringeos', u'Mastering EOS Documentation',
-     [u'Sean Fisk and Ira Woodring'], 1)
+    ('index', 'eos', u'Mastering EOS',
+     [u'Sean Fisk and Ira Woodring'], 7)
 ]
 
 # If true, show URL addresses after external links.
@@ -243,8 +243,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'MasteringEOS', u'Mastering EOS Documentation',
-   u'Sean Fisk and Ira Woodring', 'MasteringEOS', 'One line description of project.',
+  ('index', 'eos', u'Mastering EOS',
+   u'Sean Fisk and Ira Woodring', 'MasteringEOS', 'EOS lab documentation',
    'Miscellaneous'),
 ]
 
