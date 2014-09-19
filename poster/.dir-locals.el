59669ea00f0894efabc5b88220f68353a3c61c6c
Sean Fisk <sean@seanfisk.com>
2014-09-19 18:25:23 -0400

    Fix Dir Locals for AUCTeX verbatim environments.

--- poster/.dir-locals.el
+++ poster/.dir-locals.el
@@ -1,12 +1,20 @@
 ;;; Directory Local Variables
 ;;; See Info node `(emacs) Directory Variables' for more information.
 
+;; Adapted from http://stackoverflow.com/a/11474657
+
 ((latex-mode
   (TeX-master . "mastering-eos")
   (eval . (add-to-list
            'TeX-style-path
            (expand-file-name
             "style"
-            (file-name-directory
-             (let ((d (dir-locals-find-file ".")))
-               (if (stringp d) d (car d)))))))))
+            (let ((d (dir-locals-find-file ".")))
+               (if (stringp d)
+                   ;; dir-locals-find-file can return a string, which
+                   ;; will be the path to the .dir-locals.el file.
+                   (file-name-directory d)
+                 ;; dir-locals-find-file can also return a list, in
+                 ;; which case the first element will be the directory
+                 ;; containing the .dir-locals.el file.
+                 (car d))))))))
