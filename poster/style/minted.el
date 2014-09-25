c50eb125c5070d49ae2cb51f839783dd14e85e93
Sean Fisk <sean@seanfisk.com>
2014-09-25 16:07:27 -0400

    Fix previously-unnoticed poster error with gobble.

--- poster/style/minted.el
+++ poster/style/minted.el
@@ -4,6 +4,7 @@
    (message "ran minted")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "bash")
    (add-to-list 'LaTeX-verbatim-environments-local "bashcode")
+   (add-to-list 'LaTeX-verbatim-environments-local "bashcode*")
    (when (and (featurep 'font-latex)
               (eq TeX-install-font-lock 'font-latex-setup))
      (font-latex-set-syntactic-keywords)
