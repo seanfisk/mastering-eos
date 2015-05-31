77893046c998baa918ba1a0ae48962efc5247650
Sean Fisk <sean@seanfisk.com>
2015-05-31 11:14:41 -0400

    Add GVSU favicon.
    Closes part of #30.

--- manual/conf.py
+++ manual/conf.py
@@ -57,6 +57,18 @@ html_theme = 'sphinx_rtd_theme'
 # "<project> v<release> documentation".
 html_title = project
 
+# The name of an image file (within the static path) to use as favicon of the
+# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# pixels large.
+#
+# Since we are GVSU-approved, we are using the file from
+# <http://gvsu.edu/favicon.ico>.
+#
+# Although this file is not in the static path, it will be copied to it
+# automatically by Sphinx. We didn't put it in the static path to allow Sphinx
+# to put it wherever it wants.
+html_favicon = 'favicon.ico'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
